85012f21c0f966458db31ea66402307746c5c3a9
Evgeny Chaban <hi@evg.moe>
2025-01-28 23:41:30 +0300

    wip: init KMenu

---
+++ src/components/KMenu.vue
@@ -0,0 +1,296 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import KLabel from './KLabel.vue'
+import type { KLabelProps } from './KLabel.vue'
+
+export interface MenuItem {
+    text: string
+    note?: string
+    label?: KLabelProps
+}
+export interface KMenuProps {
+    items?: MenuItem[]
+    mode?: 'muted' | 'primary' | 'light'
+    weight?: 'normal' | 'semibold' | 'strong'
+    size?: 'small' | 'medium' | 'normal' | 'large'
+    underline?: boolean
+    stacked?: boolean
+    numbered?: boolean
+    numbersPosition?: 'left' | 'right'
+    pills?: boolean
+}
+
+const props = defineProps<KMenuProps>()
+
+const classes = computed(() => [
+  props.mode ?? '',
+  props.weight,
+  props.size,
+  {
+    underline: props.underline,
+    stacked: props.stacked,
+    numbered: props.numbered,
+    'numbered-right': props.numbersPosition === 'right',
+    pills: props.pills,
+  }
+])
+</script>
+
+<template>
+  <nav class="menu" :class="classes">
+    <ul class="menu-list">
+        <li v-for="item in items" class="menu-item">
+            <template v-if="!item.label">
+                <a href="#" class="menu-link">{{ item.text}}</a>
+                <p v-if="item.note">{{ item.note }}</p>
+            </template>
+            <template v-else>
+            <div class="menu-link-box">
+                <a href="#" class="menu-link flex-none">{{ item.text}}</a>
+                <KLabel v-bind="item.label" style="margin-left: 10px;" />
+            </div>
+            </template>
+        </li>
+    </ul>
+</nav>
+</template>
+
+<style scoped>
+.flex-none {
+    flex: 0 0 auto !important;
+}
+.menu {
+  --menu-link-color: var(--palette-black);
+  --menu-link-background-color: transparent;
+  --menu-link-hover-color: var(--palette-black-60);
+  --menu-link-hover-background-color: transparent;
+  --menu-link-active-color: var(--palette-black-40);
+  --menu-link-active-background-color: transparent;
+  --menu-font-size: var(--type-scale-15);
+  --menu-nested-margin: 5rem;
+  --menu-line-height: 1.3;
+  --menu-link-padding-x: 0;
+  --menu-link-padding-y: 6px;
+  --menu-link-font-weight: normal;
+  --menu-link-active-font-weight: normal;
+  --menu-item-space: 0;
+  --menu-item-padding: 0;
+  font-size: var(--menu-font-size);
+  line-height: var(--menu-line-height);
+}
+.menu .menu-item {
+  padding: var(--menu-item-padding);
+  margin-bottom: var(--menu-item-space);
+}
+.menu .menu-item:last-child {
+  margin-bottom: 0;
+}
+.menu .menu-item.active .menu-link {
+  text-decoration: none;
+  font-weight: var(--menu-link-active-font-weight);
+  color: var(--menu-link-active-color);
+  background-color: var(--menu-link-active-background-color);
+}
+.menu .menu-link {
+  display: flex;
+  align-items: center;
+  flex: 1;
+  text-decoration: none;
+  font-weight: var(--menu-link-font-weight);
+  padding-top: var(--menu-link-padding-y);
+  padding-bottom: var(--menu-link-padding-y);
+  padding-left: var(--menu-link-padding-x);
+  padding-right: var(--menu-link-padding-x);
+  color: var(--menu-link-color);
+  background-color: var(--menu-link-background-color);
+  cursor: pointer;
+}
+.menu .menu-link:hover {
+  text-decoration: underline;
+  color: var(--menu-link-hover-color);
+  background-color: var(--menu-link-hover-background-color);
+}
+.menu .menu-link-box {
+  display: flex;
+  align-items: center;
+}
+.menu .menu {
+  margin-left: var(--menu-nested-margin);
+}
+.menu .menu-flat {
+  margin-left: 0;
+}
+.menu .menu-list + h4,
+.menu .menu-list + h5,
+.menu .menu-list + h6 {
+  margin-top: 5rem;
+}
+.menu :deep(.label) {
+  margin-top: -2px;
+}
+
+.menu.large {
+  --menu-font-size: var(--type-scale-18);
+}
+
+.menu.medium {
+  --menu-font-size: var(--type-scale-14);
+}
+
+.menu.small {
+  --menu-font-size: var(--type-scale-13);
+}
+
+.menu.semibold .menu-link {
+  --menu-link-font-weight: var(--font-weight-semibold);
+  --menu-link-active-font-weight: var(--font-weight-semibold);
+}
+.menu.semibold .caret {
+  --caret-thickness: 2px;
+}
+
+.menu.strong .menu-link {
+  --menu-link-font-weight: bold;
+  --menu-link-active-font-weight: bold;
+}
+.menu.strong .caret {
+  --caret-thickness: 2px;
+}
+
+.menu.underline .menu-link {
+  text-decoration: underline;
+}
+
+.menu.underline-off .menu-link:hover {
+  text-decoration: none;
+}
+
+.menu.primary {
+  --menu-link-color: var(--palette-primary-dark);
+  --menu-link-hover-color: var(--palette-primary-darker);
+  --menu-link-active-color: var(--palette-black-40);
+}
+
+.menu.muted {
+  --menu-link-color: var(--palette-black-60);
+  --menu-link-hover-color: var(--palette-black);
+  --menu-link-active-color: var(--palette-black);
+}
+
+.menu.light {
+  --menu-link-color: var(--palette-white-80);
+  --menu-link-hover-color: var(--palette-white);
+  --menu-link-active-color: var(--palette-white-50);
+}
+
+.menu.stacked {
+  --menu-item-border-color: var(--palette-black-7);
+  --menu-link-padding-y: 8px;
+  --menu-item-border-width: 1px;
+  --menu-item-border-style: solid;
+}
+.menu.stacked .menu-item {
+  border-bottom-width: var(--menu-item-border-width);
+  border-bottom-style: var(--menu-item-border-style);
+  border-bottom-color: var(--menu-item-border-color);
+}
+.menu.stacked .menu-item:last-child {
+  border-bottom-color: transparent;
+}
+
+.menu.stacked.menu-light {
+  --menu-item-border-color: var(--palette-white-20);
+}
+
+.menu.pills {
+  --menu-link-color: var(--palette-black);
+  --menu-link-hover-background-color: var(--palette-black-10);
+  --menu-link-active-color: var(--palette-neutral-lightest);
+  --menu-link-active-background-color: var(--palette-black);
+  --menu-link-padding-y: 6px;
+  --menu-link-padding-x: 10px;
+  --menu-link-border-radius: var(--radius-base);
+  --menu-item-space: 1px;
+}
+.menu.pills .menu-link {
+  border-radius: var(--menu-link-border-radius);
+}
+.menu.pills .menu-link:hover {
+  text-decoration: none;
+}
+
+.menu.numbered {
+  --menu-number-color: var(--palette-black-60);
+  --menu-number-font-size: 80%;
+  --menu-number-width: 22px;
+}
+.menu.numbered .menu-list {
+  counter-reset: numbers;
+}
+.menu.numbered .menu-item {
+  display: flex;
+  align-items: center;
+  counter-increment: numbers;
+}
+.menu.numbered .menu-item:before {
+  width: var(--menu-number-width);
+  font-size: var(--menu-number-font-size);
+  content: counter(numbers, decimal-leading-zero);
+  color: var(--menu-number-color);
+}
+
+.menu.numbered.menu-light {
+  --menu-number-color: var(--palette-white-60);
+}
+
+.menu.numbered-right .menu-item:before {
+  display: none;
+}
+.menu.numbered-right .menu-item:after {
+  width: var(--menu-number-width);
+  font-size: var(--menu-number-font-size);
+  content: counter(numbers, decimal-leading-zero);
+  text-align: right;
+}
+
+@media (prefers-color-scheme: dark) {
+  .menu {
+    --menu-link-color: var(--palette-white-90);
+    --menu-link-background-color: transparent;
+    --menu-link-hover-color: var(--palette-white);
+    --menu-link-hover-background-color: transparent;
+    --menu-link-active-color: var(--palette-white-50);
+    --menu-link-active-background-color: transparent;
+  }
+  .menu.stacked,
+  .menu.stacked.light {
+    --menu-item-border-color: var(--palette-white-15);
+  }
+  .menu.pills,
+  .menu.pills.light {
+    --menu-link-color: var(--palette-white-90);
+    --menu-link-hover-background-color: var(--palette-white-15);
+    --menu-link-active-color: var(--palette-black);
+    --menu-link-active-background-color: var(--palette-white-90);
+  }
+  .menu.numbered,
+  .menu.numbered.light {
+    --menu-number-color: var(--palette-white-50);
+  }
+  .menu.primary {
+    --menu-link-color: var(--palette-primary-light);
+    --menu-link-hover-color: var(--palette-white-90);
+    --menu-link-active-color: var(--palette-white-90);
+  }
+  .menu.muted {
+    --menu-link-color: var(--palette-white-60);
+    --menu-link-hover-color: var(--palette-white);
+    --menu-link-active-color: var(--palette-white);
+  }
+  .menu.light {
+    --menu-link-color: var(--palette-white-80);
+    --menu-link-hover-color: var(--palette-white);
+    --menu-link-active-color: var(--palette-white-50);
+  }
+}
+</style>
