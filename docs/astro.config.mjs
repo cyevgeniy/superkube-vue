25598fafdcb1631b25cea111fe3a11b6617dd9c1
Evgeny Chaban <hi@evg.moe>
2025-02-09 20:19:48 +0300

    wip: add button examples

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,29 +2,29 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					autogenerate: { directory: 'guides' },
-				},
-				{
-					label: 'Components',
-					autogenerate: { directory: 'components' },
-				},
-				
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    integrations: [starlight({
+        title: 'My Docs',
+        social: {
+            github: 'https://github.com/withastro/starlight',
+        },
+        sidebar: [
+            {
+                label: 'Guides',
+                autogenerate: { directory: 'guides' },
+            },
+            {
+                label: 'Components',
+                autogenerate: { directory: 'components' },
+            },
+            
+            {
+                label: 'Reference',
+                autogenerate: { directory: 'reference' },
+            },
+        ],
+		}), vue()],
 });
--- docs/src/content/docs/guides/getting-started.md
+++
@@ -1,6 +0,0 @@
----
-title: Getting Started
----
-
-Need to add docs here.
-
