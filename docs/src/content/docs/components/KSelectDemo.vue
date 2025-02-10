3a50062ce18ce10d2e3119cde48192c2541c028d
Evgeny Chaban <hi@evg.moe>
2025-02-11 00:03:09 +0300

    docs: update docs

---
+++ docs/src/content/docs/components/KSelectDemo.vue
@@ -0,0 +1,12 @@
+<script setup>
+import KSelect from '../../../../../src/components/KSelect.vue';
+
+const options = [
+  { label: 'Tokyo', value: 'tokyo' },
+  { label: 'Moscow', value: 'moscow' },
+];
+</script>
+
+<template>
+  <KSelect :options="options" label="Choose a city" class="not-content" />
+</template>
---
+++ docs/src/content/docs/guides/getting-started.md
@@ -0,0 +1,7 @@
+---
+title: Getting Started
+---
+
+Superkube-vue is a component library for Vue 3 based on the Superkube design system.
+
+The documentation is still in progress.
