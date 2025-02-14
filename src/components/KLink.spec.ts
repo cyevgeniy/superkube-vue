29279f5afd05e43c51fb5e1b6cf91263c55e91bb
Evgeny Chaban <hi@evg.moe>
2025-02-14 23:19:12 +0300

    feat: add KLink component

---
+++ src/components/KLink.spec.ts
@@ -0,0 +1,51 @@
+import { mount, config } from '@vue/test-utils'
+import { describe, expect, test } from 'vitest' 
+import { createRouter, createMemoryHistory, RouterLink } from 'vue-router'
+import KLink from './KLink.vue'
+
+function setupRouter(): void {
+  const router = createRouter({
+    history: createMemoryHistory(),
+    routes: [{
+      path: '/',
+      component: { render: () => null }
+    }]
+  })
+
+  config.global.plugins.push(router)
+}
+
+beforeAll(() => setupRouter())
+
+describe('KLink component', () => {
+    it('renders as "span" when href is not provided"', () => {
+        const wrapper = mount(KLink)
+
+        expect(wrapper.find('span').exists()).toBe(true)
+    })
+
+    it('renders as "router-link" when href is not external"', () => {
+        const wrapper = mount(KLink, {
+            props: {
+                href: '/about'
+            }
+        })
+
+        expect(wrapper.findComponent(RouterLink).exists()).toBe(true)
+    })
+
+
+    it('renders as "a" when href is external"', () => {
+        let link = 'http://example.com'
+        const wrapper = mount(KLink, {
+            props: {
+                href: link
+            }
+        })
+
+        expect(wrapper.find('a').exists()).toBe(true)
+        expect(wrapper.get('a').attributes('target')).toBe('_blank')
+        expect(wrapper.get('a').attributes('href')).toBe(link)
+    })
+})
+
---
+++ src/components/KLink.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+export interface KLinkProps {
+  href?: string
+}
+
+const props = withDefaults(defineProps<KLinkProps>(), {
+  href: ''
+})
+
+const isExternal = computed(() => props.href.startsWith('http'))
+
+const as = computed(() => props.href ? isExternal.value ? 'a' : 'router-link' : 'span')
+
+// target = '_blank' implies rel=noopener
+const target = computed(() => isExternal.value ? '_blank' : undefined)
+const to = computed(() => isExternal.value ? undefined : props.href)
+const href = computed(() => isExternal.value ? props.href : undefined)
+
+
+</script>
+
+<template>
+  <component :is="as" :to="to" :href="href" :target="target" />
+</template>
