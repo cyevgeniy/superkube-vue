e4e0fa5e3db9f525e5a5aa927bf617767c2712dd
Evgeny Chaban <hi@evg.moe>
2025-03-15 23:01:31 +0300

    fix(KBaseInput): add missing styles

--- src/components/KBaseInput.vue
+++ src/components/KBaseInput.vue
@@ -1,15 +1,14 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 
-
 export interface KBaseInputProps {
-    label?: string
-    hint?: string
-    required?: boolean
-    hintPosition?: HintPosition
-    light?: boolean
-    state?: State
-    error?: string
+  label?: string
+  hint?: string
+  required?: boolean
+  hintPosition?: HintPosition
+  light?: boolean
+  state?: State
+  error?: string
 }
 
 export type State = 'neutral' | 'success' | 'error'
@@ -17,135 +16,157 @@ export type HintPosition = 'top' | 'bottom'
 export type KInputSize = 'small' | 'regular' | 'large'
 
 const props = withDefaults(defineProps<KBaseInputProps>(), {
-    hintPosition: 'bottom'
+  hintPosition: 'bottom',
 })
 
 const showTopHint = computed(() => props.hint && props.hintPosition === 'top')
 const showBottomHint = computed(() => props.hint && props.hintPosition === 'bottom')
 
-const hintClasses = computed(() => [
-    props.state || 'neutral',
-    props.light && 'light',
-])
-
+const hintClasses = computed(() => [props.state || 'neutral', props.light && 'light'])
 </script>
 
 <template>
-    <div data-testid="baseInput">
-        <label v-if="label" data-testid="inputLabel">
-            {{ label }}
-            <span v-if="props.required" class="hint req" data-testid="reqHint">*</span>
-            <span v-if="showTopHint" class="hint" :class="hintClasses" data-testid="topHint">
-                {{ hint }}
-            </span>
-        </label>
-        <div class="input-group">
-            <span v-if="$slots.prependAddon" class="input-addon">
-                <slot name="prependAddon" />
-            </span>
+  <div data-testid="baseInput">
+    <label v-if="label" data-testid="inputLabel">
+      {{ label }}
+      <span v-if="props.required" class="hint req" data-testid="reqHint">*</span>
+      <span v-if="showTopHint" class="hint" :class="hintClasses" data-testid="topHint">
+        {{ hint }}
+      </span>
+    </label>
+    <div class="input-group">
+      <span v-if="$slots.prependAddon" class="input-addon">
+        <slot name="prependAddon" />
+      </span>
 
-            <slot />
-            
-            <span v-if="$slots.appendAddon" class="input-addon">
-                <slot name="appendAddon" />
-            </span>
-        </div>
-        <div v-if="showBottomHint" class="hint" :class="hintClasses" data-testid="bottomHint"> {{ hint }} </div>
+      <slot />
+
+      <span v-if="$slots.appendAddon" class="input-addon">
+        <slot name="appendAddon" />
+      </span>
     </div>
+    <div v-if="showBottomHint" class="hint" :class="hintClasses" data-testid="bottomHint">{{ hint }}</div>
+  </div>
 </template>
 
 <style scoped>
 .input-group {
-    display: flex;
+  display: flex;
 }
 
 .input-group :deep(.input) {
-    flex: 1;
-    margin-left: -1px;
-    margin-right: -1px;
+  flex: 1;
+  margin-left: -1px;
+  margin-right: -1px;
 }
 
-.input-group> :not(:last-child) {
-    border-top-right-radius: 0;
-    border-bottom-right-radius: 0;
+.input-group > :not(:last-child) {
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
 }
 
-.input-group> :not(:first-child) {
-    border-top-left-radius: 0;
-    border-bottom-left-radius: 0;
+.input-group > :not(:first-child) {
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
 }
 
 .hint {
-    --hint-color: var(--palette-black-50);
-    --hint-font-size: var(--type-scale-13);
-    --hint-font-weight: normal;
-    --hint-font-style: normal;
-    --hint-line-height: var(--body-text-line);
-    font-size: var(--hint-font-size);
-    font-weight: var(--hint-font-weight);
-    font-style: var(--hint-font-style);
-    line-height: var(--hint-line-height);
-    color: var(--hint-color);
+  --hint-color: var(--palette-black-50);
+  --hint-font-size: var(--type-scale-13);
+  --hint-font-weight: normal;
+  --hint-font-style: normal;
+  --hint-line-height: var(--body-text-line);
+  font-size: var(--hint-font-size);
+  font-weight: var(--hint-font-weight);
+  font-style: var(--hint-font-style);
+  line-height: var(--hint-line-height);
+  color: var(--hint-color);
 }
 
 label .hint {
-    text-transform: none;
-    font-weight: normal;
-    letter-spacing: 0;
+  text-transform: none;
+  font-weight: normal;
+  letter-spacing: 0;
 }
 
 /** Add a gap between required hint and an ordinary hint with position='top' */
 label .hint + .hint {
-    margin-left: 4px;
+  margin-left: 4px;
 }
 
 div.hint {
-    margin-top: 2px;
+  margin-top: 2px;
 }
 
 .hint.light {
-    --hint-color: var(--palette-white-70);
+  --hint-color: var(--palette-white-70);
 }
 
 .hint.success {
-    --hint-color: var(--palette-positive-dark);
+  --hint-color: var(--palette-positive-dark);
 }
 
 .hint.success.light {
-    --hint-color: var(--palette-positive-mid);
+  --hint-color: var(--palette-positive-mid);
 }
 
 .hint.error {
-    --hint-color: var(--palette-negative-dark);
+  --hint-color: var(--palette-negative-dark);
 }
 
 .hint.req {
-    --hint-color: var(--palette-negative-base);
-    --hint-font-weight: bold;
-    font-size: 105%;
+  --hint-color: var(--palette-negative-base);
+  --hint-font-weight: bold;
+  font-size: 105%;
 }
 
 .hint.req.light,
 .hint.error.light {
-    --hint-color: var(--palette-negative-mid);
+  --hint-color: var(--palette-negative-mid);
+}
+
+label {
+  --form-label-color: var(--text-default);
+  --form-background-color: transparent;
+  --form-label-font-size: var(--type-scale-15);
+  --form-label-line-height: var(--body-text-line);
+  --form-label-font-weight: normal;
+  --form-label-font-style: normal;
+  --form-label-text-transform: none;
+  --form-label-letter-spacing: 0;
+  --form-label-space: 0.15em;
+  --form-label-border-radius: 0;
+  --form-label-padding: 0;
+  display: block;
+  font-size: var(--form-label-font-size);
+  font-weight: var(--form-label-font-weight);
+  font-style: var(--form-label-font-style);
+  line-height: var(--form-label-line-height);
+  text-transform: var(--form-label-text-transform);
+  letter-spacing: var(--form-label-letter-spacing);
+  margin-bottom: var(--form-label-space);
+  border-radius: var(--form-label-border-radius);
+  padding: var(--form-padding);
+  color: var(--form-label-color);
+  background-color: var(--form-label-background-color);
 }
 
 @media (prefers-color-scheme: dark) {
-    .hint {
-        --hint-color: var(--palette-white-50);
-    }
+  .hint {
+    --hint-color: var(--palette-white-50);
+  }
 
-    .hint.success {
-        --hint-color: var(--palette-positive-mid);
-    }
+  .hint.success {
+    --hint-color: var(--palette-positive-mid);
+  }
 
-    .hint.error {
-        --hint-color: var(--palette-negative-mid);
-    }
+  .hint.error {
+    --hint-color: var(--palette-negative-mid);
+  }
 
-    .hint.req {
-        --hint-color: var(--palette-negative-mid);
-    }
+  .hint.req {
+    --hint-color: var(--palette-negative-mid);
+  }
 }
 
 .input-addon {
