a13487fe467af141009a97c26310e7bb5b82634b
Evgeny Chaban <hi@evg.moe>
2025-03-15 23:31:56 +0300

    chore: add github actions

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,30 +1,34 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
 
-import vue from '@astrojs/vue';
+import vue from '@astrojs/vue'
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [starlight({
-        title: 'My Docs',
-        social: {
-            github: 'https://github.com/withastro/starlight',
+  base: '/superkube-vue/docs',
+  integrations: [
+    starlight({
+      title: 'Superkube Vue',
+      social: {
+        github: 'https://github.com/withastro/starlight',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          autogenerate: { directory: 'guides' },
         },
-        sidebar: [
-            {
-                label: 'Guides',
-                autogenerate: { directory: 'guides' },
-            },
-            {
-                label: 'Components',
-                autogenerate: { directory: 'components' },
-            },
-            
-            {
-                label: 'Reference',
-                autogenerate: { directory: 'reference' },
-            },
-        ],
-		}), vue()],
-});
+        {
+          label: 'Components',
+          autogenerate: { directory: 'components' },
+        },
+
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+    vue(),
+  ],
+})
