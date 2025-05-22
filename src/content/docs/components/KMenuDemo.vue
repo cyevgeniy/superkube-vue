fb7b7dde6b5456a865bf6cb1ea87ea505529d3d5
Evgeny Chaban <hi@evg.moe>
2025-05-22 23:26:56 +0300

    docs: update

---
+++ src/content/docs/components/KMenuDemo.vue
@@ -0,0 +1,16 @@
+<script setup>
+import ComponentExample from './ComponentExample.vue'
+import KMenu from '../../../components/KMenu.vue'
+
+const items = [
+  { text: 'Tokyo', note: { text: 'test', color: 'primary' } },
+  { text: 'Moscow' },
+  { text: 'London', note: { text: 'Pick this if you want', color: 'primary', size: 'micro' } },
+]
+</script>
+
+<template>
+  <ComponentExample>
+    <KMenu :items="items" weight="strong" stacked numbered class="not-content" />
+  </ComponentExample>
+</template>
---
+++ src/content/docs/components/KTextDemo.vue
@@ -0,0 +1,28 @@
+<script setup>
+import ComponentExample from './ComponentExample.vue'
+import KText from '../../../components/KText.vue'
+
+const colors = [
+  'default',
+  'primary',
+  'dark',
+  'dark-mid',
+  'muted',
+  'success',
+  'error',
+  'light',
+  'light-mid',
+  'light-muted',
+]
+</script>
+
+<template>
+  <ComponentExample>
+    <template v-for="color in colors" :key="color">
+      <KText :color="color" class="not-content"> {{ color }} text </KText>
+    </template>
+    <KText mono> mono text </KText>
+    <KText italic> Italic text </KText>
+    <KText caps> caps text </KText>
+  </ComponentExample>
+</template>
