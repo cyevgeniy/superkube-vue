60f11ae67c506059c60c9a344a86ee03bffbdb0a
Evgeny Chaban <hi@evg.moe>
2025-05-26 21:54:50 +0300

    wip: vitepress

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
           { text: 'KBreadcrumb', link: '/KBreadcrumb' },
           { text: 'KDivider', link: '/KDivider' },
           { text: 'KLabel', link: '/KLabel' },
+          { text: 'KTextField', link: '/KTextField' },
         ],
       },
     ],
