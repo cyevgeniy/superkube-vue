5e583bdddef3a9bce5b7d3d906b42c17090d21f5
Evgeny Chaban <hi@evg.moe>
2025-06-12 20:21:23 +0300

    fix: tests

--- src/components/KLink.spec.ts
+++ src/components/KLink.spec.ts
@@ -1,15 +1,17 @@
 import { mount, config } from '@vue/test-utils'
-import { describe, expect, test } from 'vitest' 
+import { describe, expect, it, beforeAll } from 'vitest'
 import { createRouter, createMemoryHistory, RouterLink } from 'vue-router'
 import KLink from './KLink.vue'
 
 function setupRouter(): void {
   const router = createRouter({
     history: createMemoryHistory(),
-    routes: [{
-      path: '/',
-      component: { render: () => null }
-    }]
+    routes: [
+      {
+        path: '/',
+        component: { render: () => null },
+      },
+    ],
   })
 
   config.global.plugins.push(router)
@@ -18,34 +20,32 @@ function setupRouter(): void {
 beforeAll(() => setupRouter())
 
 describe('KLink component', () => {
-    it('renders as "span" when href is not provided"', () => {
-        const wrapper = mount(KLink)
+  it('renders as "span" when href is not provided"', () => {
+    const wrapper = mount(KLink)
 
-        expect(wrapper.find('span').exists()).toBe(true)
+    expect(wrapper.find('span').exists()).toBe(true)
+  })
+
+  it('renders as "router-link" when href is not external"', () => {
+    const wrapper = mount(KLink, {
+      props: {
+        href: '/about',
+      },
     })
 
-    it('renders as "router-link" when href is not external"', () => {
-        const wrapper = mount(KLink, {
-            props: {
-                href: '/about'
-            }
-        })
+    expect(wrapper.findComponent(RouterLink).exists()).toBe(true)
+  })
 
-        expect(wrapper.findComponent(RouterLink).exists()).toBe(true)
+  it('renders as "a" when href is external"', () => {
+    let link = 'http://example.com'
+    const wrapper = mount(KLink, {
+      props: {
+        href: link,
+      },
     })
 
-
-    it('renders as "a" when href is external"', () => {
-        let link = 'http://example.com'
-        const wrapper = mount(KLink, {
-            props: {
-                href: link
-            }
-        })
-
-        expect(wrapper.find('a').exists()).toBe(true)
-        expect(wrapper.get('a').attributes('target')).toBe('_blank')
-        expect(wrapper.get('a').attributes('href')).toBe(link)
-    })
+    expect(wrapper.find('a').exists()).toBe(true)
+    expect(wrapper.get('a').attributes('target')).toBe('_blank')
+    expect(wrapper.get('a').attributes('href')).toBe(link)
+  })
 })
-
